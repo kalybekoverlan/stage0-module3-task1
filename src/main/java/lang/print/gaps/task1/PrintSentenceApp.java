891aab34adb3b684165b3a37c4d39db469e71566
erlan <kalybekov.erlan@gmail.com>
2022-06-23 19:30:16 +0600

    Fixed

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        System.out.print("This is my first java program");
+        System.out.print("This is my first Java program");
     }
 }
